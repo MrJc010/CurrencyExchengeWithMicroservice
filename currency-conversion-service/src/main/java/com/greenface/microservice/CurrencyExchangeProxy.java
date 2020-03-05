2be68f87dcc33de384ff20d6b45112a899199e5c
MrJc010 <32074443+MrJc010@users.noreply.github.com>
2020-03-04 20:32:18 -0800

    finish microservice

--- currency-conversion-service/src/main/java/com/greenface/microservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/greenface/microservice/CurrencyExchangeProxy.java
@@ -1,10 +1,12 @@
 package com.greenface.microservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange-service",url="http://localhost:8000")
+@FeignClient(name="currency-exchange-service")
+@RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeProxy {
 	@GetMapping("/getchange/{from}-{to}")
 	public CurrencyConversion exchange(@PathVariable String from,@PathVariable String to);
